88111244b71f8d3ce77ef703c33ccb5cc5781449
Thai Huynh <rafehuynh0510@gmail.com>
2023-10-30 16:03:09 -0500

    School element

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import School from "./components/School";
 
 function App() {
   return (
     <div >
       <Navbar/>
       <Home/>
+      <School/>
     </div>
   );
 }
